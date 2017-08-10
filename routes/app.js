9f7d8ace9d091d10ba9d454e8bd434e8523f22ed
Steve Witman <stevewitman@gmail.com>
2017-08-09 23:46:40 -0600

    Use mongoose to fetch and store data

--- routes/app.js
+++ routes/app.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user')
 
 router.get('/', function (req, res, next) {
-    res.render('index');
-});
-
-router.get('/message/:msg', function (req, res, next) {
-    res.render('node', {
-        message: req.params.msg
+    User.findOne({}, function(err, doc) {
+        if (err)  {
+            return res.send('Error!');
+        }
+        res.render('node', {email: doc.email});
     });
 });
 
-router.post('/message', function (req, res, next) {
-    var message = req.body.message;
-    res.redirect('/message/' + message);
+router.post('/', function (req, res, next) {
+    var email = req.body.email;
+    var user = new User({
+        firstName: 'TestFirst',
+        lastName: 'TestLast',
+        password: 'super-secret',
+        email: email
+    });
+    user.save();
+    res.redirect('/');
 });
 
 module.exports = router;
